a8bf04693d376111d15adf744aa40b3b36aafb9d
unknown <rrrr2098@yahoo.com>
2024-09-14 14:14:51 +0800

    HW

--- HW/oakME.js
+++ HW/oakME.js
@@ -15,6 +15,7 @@ app.use((ctx) => {
           <li><a href="/name">姓名</a></li>
           <li><a href="/age">年齡</a></li>
           <li><a href="/gender">性別</a></li>
+          <li><a href="/intro">自我介紹</a></li>
         </ol>
       </body>
       </html>
@@ -25,6 +26,8 @@ app.use((ctx) => {
     ctx.response.body = '21';
   } else if (pathname == '/gender') {
     ctx.response.body = '男';
+  } else if (pathname == '/intro') {
+    ctx.response.body = '大家好，我叫吳承泰，今年21歲，目前是大學四年級的學生，主修的是資訊工程學系。';
   } else {
     ctx.response.status = 404;
     ctx.response.body = 'Not Found!';
